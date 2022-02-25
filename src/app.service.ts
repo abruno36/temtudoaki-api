6029a29dd3ff543940e44d8e5e9dee2fb99965eb
Bruno <pj_antoniobruno@bocombbm.com.br>
2022-02-25 16:44:13 -0300

    Commit Inicial API - 25021644

---
+++ src/app.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello Antonio Bruno!';
+  }
+
+  getObject(): any {
+    let objeto = {
+      id: 1,
+      nome: "Antonio Bruno",
+      email: "antonio.a.bruno@avanade.com",
+      nacionalidade: "Brasileira",
+      sexo: "M",
+      telefone: "11993936883"
+    }
+    return objeto
+  }
+}
