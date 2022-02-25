4a8c6104f10b9a1b0f5ee5019f0a79eab5fa0dfc
Bruno <pj_antoniobruno@bocombbm.com.br>
2022-02-25 16:45:51 -0300

    Atualizações API 25021645

--- src/app.service.ts
+++ src/app.service.ts
@@ -13,7 +13,8 @@ export class AppService {
       email: "antonio.a.bruno@avanade.com",
       nacionalidade: "Brasileira",
       sexo: "M",
-      telefone: "11993936883"
+      telefone: "11993936883",
+      celular: "993933999"
     }
     return objeto
   }
