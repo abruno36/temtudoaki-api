6fcb5d2954fca34c0ed92f6e1d60cc0d0af82014
Bruno <pj_antoniobruno@bocombbm.com.br>
2022-03-31 12:24:04 -0300

    Atualizações - 31031224

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,7 +1,11 @@
-import { Injectable, Inject, HttpException, HttpStatus, forwardRef } from '@nestjs/common';
-import { ResultadoDto } from 'src/dto/resultado.dto';
+import {
+  Injectable,
+  Inject,
+  HttpException,
+  HttpStatus,
+  forwardRef,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
-import * as bcrypt from 'bcrypt';
 import { Token } from './token.entity';
 import { UsuarioService } from 'src/usuario/usuario.service';
 import { AuthService } from 'src/auth/auth.service';
@@ -14,43 +18,48 @@ export class TokenService {
     private tokenRepository: Repository<Token>,
     private usuarioService: UsuarioService,
     @Inject(forwardRef(() => AuthService))
-    private authService: AuthService
+    private authService: AuthService,
   ) {}
 
-  async save(hash: string, username: string){
-    let objToken = await this.tokenRepository.findOne({username: username})
-    if (objToken){
+  async save(hash: string, username: string) {
+    const objToken = await this.tokenRepository.findOne({ username: username });
+    if (objToken) {
       this.tokenRepository.update(objToken.id, {
-        hash: hash
-      })
-    }else{
+        hash: hash,
+      });
+    } else {
       this.tokenRepository.insert({
         hash: hash,
-        username: username
-      })
+        username: username,
+      });
     }
   }
 
-  async refreshToken(oldToken: string){
-    let objToken = await this.tokenRepository.findOne({ hash: oldToken })
-    if (objToken){
-      let usuario = await this.usuarioService.findOne(objToken.username)      
-      return this.authService.login(usuario)
-    }else{ //é uma requisição inválida
-      return new HttpException({
-        errorMessage: 'Token inválido'
-      }, HttpStatus.UNAUTHORIZED)
+  async refreshToken(oldToken: string) {
+    const objToken = await this.tokenRepository.findOne({ hash: oldToken });
+    if (objToken) {
+      const usuario = await this.usuarioService.findOne(objToken.username);
+      return this.authService.login(usuario);
+    } else {
+      //é uma requisição inválida
+      return new HttpException(
+        {
+          errorMessage: 'Token inválido',
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
     }
   }
 
-  async getUsuarioByToken(token: string): Promise<Usuario>{
-    token = token.replace("Bearer ","").trim()
-    let objToken: Token = await this.tokenRepository.findOne({hash: token})
-    if (objToken){
-      let usuario = await this.usuarioService.findOne(objToken.username)      
-      return usuario
-    }else{ //é uma requisição inválida
-      return null
+  async getUsuarioByToken(token: string): Promise<Usuario> {
+    token = token.replace('Bearer ', '').trim();
+    const objToken: Token = await this.tokenRepository.findOne({ hash: token });
+    if (objToken) {
+      const usuario = await this.usuarioService.findOne(objToken.username);
+      return usuario;
+    } else {
+      //é uma requisição inválida
+      return null;
     }
   }
 }
